251bebfcc09a9b2510312b81c495e87058ed1299
gabrielmoos <91801383+gabrielmoos@users.noreply.github.com>
2022-03-09 23:17:19 -0600

    Add files via upload

--- CS-Project/hello.py
+++ CS-Project/hello.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+import folium
+
 app = Flask(__name__)
+
+app.debug = True
 
 @app.route('/')
 def home():
@@ -8,6 +12,14 @@ def home():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/test/')
+def test():
+    return render_template('test.html')
+
+@app.route('/map/')
+def map():
+    return render_template('congressional_mapping.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
